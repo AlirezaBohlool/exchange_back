21e2609e75939f8e4d86d425698b016930c89beb
Alireza Bohlol <dev.alirezabohlool@gmail.com>
2025-07-22 17:05:58 +0330

    fix ip

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 
 // Enable CORS for all origins (or specify origin if you want to restrict)
-app.use(cors({ origin: 'http://45.139.10.210:3000', credentials: true }));
+app.use(cors({ origin: 'http://185.243.48.94:3000', credentials: true }));
 const port = process.env.PORT || 4000;
 
 // Middleware to parse JSON
@@ -15,7 +15,8 @@ app.use(express.json());
 // Mount central router
 const mainRouter = require('./app/router');
 app.use('/api', mainRouter);
-
+// Health check route
+app.get('/api/ping', (req, res) => res.send('pong'));
 // Start server
 app.listen(port, '0.0.0.0', () => {
     console.log(`🚀 Server running on http://0.0.0.0:${port}`);
